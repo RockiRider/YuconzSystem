4d4b965db30e5ae3e8b53d4e4c96b63990383881
T.Gvadzabia <tg289@kent.ac.uk>
2020-02-17 01:06:13 +0000

    MainDisplay

---
+++ Project/src/authApp/LoginDisplay.java
@@ -0,0 +1,92 @@
+package authApp;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+
+public class LoginDisplay {
+
+	private JFrame frmYuconzSystem;
+	private JPasswordField passwordField;
+	private JTextField textField;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginDisplay window = new LoginDisplay();
+					window.frmYuconzSystem.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginDisplay() {
+		initialize();
+	}
+
+	/**
+	 * Initialise the contents of the frame.
+	 */
+	private void initialize() {
+		frmYuconzSystem = new JFrame();
+		frmYuconzSystem.setIconImage(Toolkit.getDefaultToolkit().getImage(MainDisplay.class.getResource("/authApp/img/LogoNoText.png")));
+		frmYuconzSystem.setTitle("Yuconz System");
+		frmYuconzSystem.getContentPane().setFont(new Font("Calibri", Font.BOLD, 26));
+		frmYuconzSystem.setBounds(100, 100, 450, 301);
+		frmYuconzSystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmYuconzSystem.getContentPane().setLayout(null);
+		
+		JLabel lblTitle = new JLabel("Yuconz");
+		lblTitle.setFont(new Font("Calibri", Font.BOLD, 28));
+		lblTitle.setBounds(177, 11, 88, 27);
+		frmYuconzSystem.getContentPane().add(lblTitle);
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Calibri Light", Font.PLAIN, 15));
+		passwordField.setBounds(128, 136, 190, 27);
+		frmYuconzSystem.getContentPane().add(passwordField);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Calibri Light", Font.PLAIN, 15));
+		textField.setBounds(127, 90, 191, 27);
+		frmYuconzSystem.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnLogin = new JButton("LOGIN");
+		btnLogin.setBounds(128, 186, 88, 38);
+		frmYuconzSystem.getContentPane().add(btnLogin);
+		
+		JButton btnExit = new JButton("EXIT");
+		btnExit.setBounds(229, 186, 88, 38);
+		frmYuconzSystem.getContentPane().add(btnExit);
+		
+		lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(LoginDisplay.class.getResource("/authApp/img/user25.png")));
+		lblNewLabel.setBounds(92, 90, 25, 27);
+		frmYuconzSystem.getContentPane().add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setIcon(new ImageIcon(LoginDisplay.class.getResource("/authApp/img/lock25.png")));
+		lblNewLabel_1.setBounds(93, 136, 25, 27);
+		frmYuconzSystem.getContentPane().add(lblNewLabel_1);
+	}
+}
--- Project/src/authApp/MainDisplay.java
+++ Project/src/authApp/MainDisplay.java
@@ -3,22 +3,13 @@ package authApp;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
 import java.awt.Toolkit;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class MainDisplay {
 
-	private JFrame frmYuconzSystem;
-	private JPasswordField passwordField;
-	private JTextField textField;
-	private JLabel lblNewLabel;
-	private JLabel lblNewLabel_1;
+	private JFrame frame;
 
 	/**
 	 * Launch the application.
@@ -28,7 +19,7 @@ public class MainDisplay {
 			public void run() {
 				try {
 					MainDisplay window = new MainDisplay();
-					window.frmYuconzSystem.setVisible(true);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -47,46 +38,22 @@ public class MainDisplay {
 	 * Initialise the contents of the frame.
 	 */
 	private void initialize() {
-		frmYuconzSystem = new JFrame();
-		frmYuconzSystem.setIconImage(Toolkit.getDefaultToolkit().getImage(MainDisplay.class.getResource("/authApp/img/LogoNoText.png")));
-		frmYuconzSystem.setTitle("Yuconz System");
-		frmYuconzSystem.getContentPane().setFont(new Font("Calibri", Font.BOLD, 26));
-		frmYuconzSystem.setBounds(100, 100, 450, 301);
-		frmYuconzSystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmYuconzSystem.getContentPane().setLayout(null);
+		frame = new JFrame();
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(MainDisplay.class.getResource("/authApp/img/LogoNoText.png")));
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JLabel lblTitle = new JLabel("Yuconz");
-		lblTitle.setFont(new Font("Calibri", Font.BOLD, 28));
-		lblTitle.setBounds(177, 11, 88, 27);
-		frmYuconzSystem.getContentPane().add(lblTitle);
+		JButton btnLogout = new JButton("LOGOUT");
+		btnLogout.setBounds(174, 174, 89, 23);
+		frame.getContentPane().add(btnLogout);
 		
-		passwordField = new JPasswordField();
-		passwordField.setFont(new Font("Calibri Light", Font.PLAIN, 15));
-		passwordField.setBounds(128, 136, 190, 27);
-		frmYuconzSystem.getContentPane().add(passwordField);
+		JLabel lblWelcome = new JLabel("Welcome");
+		lblWelcome.setBounds(119, 58, 216, 23);
+		frame.getContentPane().add(lblWelcome);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Calibri Light", Font.PLAIN, 15));
-		textField.setBounds(127, 90, 191, 27);
-		frmYuconzSystem.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		JButton btnLogin = new JButton("LOGIN");
-		btnLogin.setBounds(128, 186, 88, 38);
-		frmYuconzSystem.getContentPane().add(btnLogin);
-		
-		JButton btnExit = new JButton("EXIT");
-		btnExit.setBounds(229, 186, 88, 38);
-		frmYuconzSystem.getContentPane().add(btnExit);
-		
-		lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(MainDisplay.class.getResource("/authApp/img/user25.png")));
-		lblNewLabel.setBounds(92, 90, 25, 27);
-		frmYuconzSystem.getContentPane().add(lblNewLabel);
-		
-		lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setIcon(new ImageIcon(MainDisplay.class.getResource("/authApp/img/lock25.png")));
-		lblNewLabel_1.setBounds(93, 136, 25, 27);
-		frmYuconzSystem.getContentPane().add(lblNewLabel_1);
+		JLabel lblRole = new JLabel("Role: ");
+		lblRole.setBounds(303, 11, 49, 14);
+		frame.getContentPane().add(lblRole);
 	}
 }
