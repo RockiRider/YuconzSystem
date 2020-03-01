bebfe2c8c0159925c41beeb0b53267128aabe3e1
T.Gvadzabia <tg289@kent.ac.uk>
2020-03-01 22:17:42 +0000

    finished task

---
+++ Project/src/authApp/Roles/HRDirector.java
@@ -0,0 +1,10 @@
+package authApp.Roles;
+import authApp.User;
+
+public class HRDirector extends User {
+
+	public HRDirector(String fName, String lName) {
+		super(fName, lName, "HR Director", true);
+	}
+
+}
