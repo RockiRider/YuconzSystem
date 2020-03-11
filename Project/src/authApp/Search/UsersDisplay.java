c8ea2679df3f6b5673ee1854a4c4e9bd20e28b7f
tg289 <tsotne24@hotmail.com>
2020-03-11 11:40:13 +0000

    UsersDisplay Table

--- Project/src/authApp/Search/UsersDisplay.java
+++ Project/src/authApp/Search/UsersDisplay.java
@@ -1,18 +1,17 @@
 package authApp.Search;
 
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Toolkit;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 import javax.swing.JFrame;
-
 import authApp.PD.MyPdWindow;
+import javax.swing.JLabel;
+import javax.swing.JTable;
 
 public class UsersDisplay {
 
 	private JFrame frame;
+	private JTable table;
+	private String[] columnNames = {"First Name","Last Name","Role"};
 
 
 
@@ -35,8 +34,24 @@ public class UsersDisplay {
 		frame.setBounds(100, 100, 787, 534);
 		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		JLabel lblFName = new JLabel("First Name");
+		lblFName.setBounds(10, 11, 69, 26);
+		frame.getContentPane().add(lblFName);
+		
+		JLabel lblRoles = new JLabel("Roles");
+		lblRoles.setBounds(158, 11, 69, 26);
+		frame.getContentPane().add(lblRoles);
+		
+		JLabel lblSName = new JLabel("Last Name");
+		lblSName.setBounds(87, 17, 61, 14);
+		frame.getContentPane().add(lblSName);
+		
+		table = new JTable();
+		table.setColumnSelectionAllowed(true);
+		table.setBounds(30, 62, 307, 316);
+		frame.getContentPane().add(table);
 		frame.setLocationRelativeTo(null); 
 		frame.setVisible(true);
 	}
-
 }
